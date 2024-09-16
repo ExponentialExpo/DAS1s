7b1b1e2b157aae54c3ac01c3602993703c50fddb
Arcstac <rliu0909@bths.edu>
2024-09-16 10:15:21 -0400

    Black Bean Noods are very nice :)

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ class Main
     public static void main(String[] args)
     {
         System.out.println("THIS IS ARCSTAC HII");
-        System.out.println("I made yummy jajangmyeon for lunch today");
+        System.out.println("I made yummy jajangmyeon for lunch today :D");
     }
 }
